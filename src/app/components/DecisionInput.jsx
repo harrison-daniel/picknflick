4c0f4a9899aa17a570f5c860a246ef3f514b1cb5
Harrison Daniel <hd4n@outlook.com>
2024-05-20 16:52:11 -0400

    DecisionInput final mobile Drawer adjustments

--- src/app/components/DecisionInput.jsx
+++ src/app/components/DecisionInput.jsx
@@ -11,6 +11,11 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from './ui/drawer';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '../components/ui/popover';
 import { Button } from '../components/ui/button';
 import { Label } from '../components/ui/label';
 import { Input } from '../components/ui/input';
@@ -126,81 +131,155 @@ function DecisionInput({ options, updateOptions }) {
 
   return (
     <div>
-      <Drawer open={open} onOpenChange={setOpen}>
-        <DrawerTrigger asChild>
-          <Button
-            variant='outline'
-            className='bg-zinc-400 font-bold text-gray-800'>
-            Open Options
-          </Button>
-        </DrawerTrigger>
-        <DrawerContent className='max-h-[80vh] overflow-y-auto'>
-          <DrawerHeader className='flex flex-col justify-center gap-0.5 py-1 pb-2'>
-            <DrawerTitle className='flex justify-center'>
-              Enter your decisions below!
-            </DrawerTitle>
-            <DrawerDescription className='flex flex-col justify-center'>
-              <div className='flex justify-center'>{getHeaderText()}</div>
-            </DrawerDescription>
-          </DrawerHeader>
-          {/* ------------ INPUT CONTENT START ----------*/}
-          <div className='my-2 flex flex-col gap-0.5'>
-            {tempOptions.map((option, index) => (
-              <div
-                key={index}
-                className='mx-5 my-1 flex h-7 flex-row items-center justify-between gap-3'>
-                <Label
-                  htmlFor={`option-${index}`}
-                  className='whitespace-nowrap text-center'>
-                  Option <span>{tempOptions.length - index}</span>
-                </Label>
-                <Input
-                  id={`option-${index}`}
-                  value={option}
-                  onChange={(e) => handleOptionChange(e, index)}
-                  onBlur={handleBlur}
-                  onKeyDown={(e) => handleKeyDown(e, index)}
-                  ref={(el) => (inputRefs.current[index] = el)}
-                />
-                {tempOptions.length > 2 && (
-                  <Button
-                    onClick={() => removeOption(index)}
-                    variant='danger'
-                    className='m-0 p-0'>
-                    <FaRegWindowClose />
-                  </Button>
-                )}
+      {isDesktop ? (
+        <div>
+          <Popover onClose={handleBlur}>
+            <PopoverTrigger asChild>
+              <Button
+                variant='outline'
+                className='bg-zinc-400 font-bold text-gray-800'>
+                Open Options
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className='p-4'>
+              <h1 className='z-10 flex justify-center bg-white p-2 font-bold '>
+                Enter your decisions below!
+              </h1>
+              <div className='flex justify-center text-center'>
+                {getHeaderText()}
               </div>
-            ))}
-          </div>
+              {/* <AnimatePresence> */}
+              {tempOptions.map((option, index) => (
+                <div
+                  key={index}
+                  // {...animationProps}
+                  className='m-4 flex flex-row items-center justify-between gap-3'>
+                  <Label
+                    htmlFor={`option-${index}`}
+                    className='whitespace-nowrap text-center'>
+                    Option <span>{tempOptions.length - index}</span>
+                  </Label>
+                  <Input
+                    id={`option-${index}`}
+                    value={option}
+                    onChange={(e) => handleOptionChange(e, index)}
+                    onBlur={handleBlur}
+                    onKeyDown={(e) => handleKeyDown(e, index)}
+                    ref={(el) => (inputRefs.current[index] = el)}
+                  />
+                  {tempOptions.length > 2 && (
+                    <Button
+                      onClick={() => removeOption(index)}
+                      variant='danger'
+                      className='m-0 p-0'>
+                      <FaRegWindowClose />
+                    </Button>
+                  )}
+                </div>
+              ))}
+              {/* </AnimatePresence> */}
 
-          <div className='my-2 flex flex-row justify-center gap-4'>
+              <div className='mt-4 flex flex-row justify-center gap-4'>
+                <Button
+                  onClick={handleClearOptions}
+                  variant='danger'
+                  className='text-red-500'
+                  disabled={!anyOptionFilled(tempOptions)}>
+                  Clear All
+                </Button>
+                <Button
+                  onClick={addOption}
+                  className='flex flex-row gap-1.5 border border-black p-2'
+                  variant='primary'
+                  disabled={
+                    tempOptions.length >= 6 || !allOptionsFilled(tempOptions)
+                  }>
+                  <FaPlus size={12} />
+                  Add Option
+                </Button>
+              </div>
+            </PopoverContent>
+          </Popover>
+        </div>
+      ) : (
+        <Drawer open={open} onOpenChange={setOpen}>
+          <DrawerTrigger asChild>
             <Button
-              onClick={handleClearOptions}
-              variant='danger'
-              className='h-[32px] text-red-500'
-              disabled={!anyOptionFilled(tempOptions)}>
-              Clear All
+              variant='outline'
+              className='bg-zinc-400 font-bold text-gray-800'>
+              Open Options
             </Button>
-            <Button
-              onClick={addOption}
-              className='flex h-[32px] flex-row border border-black p-1'
-              variant='primary'
-              disabled={
-                tempOptions.length >= 6 || !allOptionsFilled(tempOptions)
-              }>
-              <FaPlus size={12} />
-              Add Option
-            </Button>
-          </div>
-          {/* ------------ END INPUT  ----------*/}
-          <DrawerFooter className='pt-0'>
-            <DrawerClose asChild className='h-9'>
-              <Button variant='outline'>Close</Button>
-            </DrawerClose>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
+          </DrawerTrigger>
+          <DrawerContent className=' flex min-h-fit flex-col'>
+            <div className='my-auto min-h-fit'>
+              <DrawerHeader className='flex flex-col justify-center gap-0.5 py-1 pb-2'>
+                <DrawerTitle className='flex justify-center'>
+                  Enter your decisions below!
+                </DrawerTitle>
+                <DrawerDescription className='flex flex-col justify-center'>
+                  <div className='flex justify-center'>{getHeaderText()}</div>
+                </DrawerDescription>
+              </DrawerHeader>
+              {/* ------------ INPUT CONTENT START ----------*/}
+
+              {tempOptions.map((option, index) => (
+                <div
+                  key={index}
+                  className='mx-5 my-1 flex  flex-row items-center justify-between gap-3'>
+                  <Label
+                    htmlFor={`option-${index}`}
+                    className='whitespace-nowrap text-center'>
+                    Option <span>{tempOptions.length - index}</span>
+                  </Label>
+                  <Input
+                    className=''
+                    id={`option-${index}`}
+                    value={option}
+                    onChange={(e) => handleOptionChange(e, index)}
+                    onBlur={handleBlur}
+                    onKeyDown={(e) => handleKeyDown(e, index)}
+                    ref={(el) => (inputRefs.current[index] = el)}
+                  />
+                  {tempOptions.length > 2 && (
+                    <Button
+                      onClick={() => removeOption(index)}
+                      variant='danger'
+                      className='m-0 p-0'>
+                      <FaRegWindowClose />
+                    </Button>
+                  )}
+                </div>
+              ))}
+
+              <div className='my-2 flex flex-row justify-center gap-4'>
+                <Button
+                  onClick={handleClearOptions}
+                  variant='danger'
+                  className='h-[32px] text-red-500'
+                  disabled={!anyOptionFilled(tempOptions)}>
+                  Clear All
+                </Button>
+                <Button
+                  onClick={addOption}
+                  className='flex h-[32px] flex-row border border-black p-1'
+                  variant='primary'
+                  disabled={
+                    tempOptions.length >= 6 || !allOptionsFilled(tempOptions)
+                  }>
+                  <FaPlus size={12} />
+                  Add Option
+                </Button>
+              </div>
+              {/* ------------ END INPUT  ----------*/}
+              <DrawerFooter className='pt-0'>
+                <DrawerClose asChild>
+                  <Button variant='outline'>Close</Button>
+                </DrawerClose>
+              </DrawerFooter>
+            </div>
+          </DrawerContent>
+        </Drawer>
+      )}
     </div>
   );
 }
