eec977d0ad92bc211d04ecfc225717fa15b47420
Harrison Daniel <hd4n@outlook.com>
2024-08-13 00:00:10 -0400

    script adjustments for deployment

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,13 @@
-// jest.config.js
-const nextJest = require('next/jest');
+// const nextJest = require('next/jest');
 
-const createJestConfig = nextJest({
-  dir: './', // Path to your Next.js app directory
-});
+// const createJestConfig = nextJest({
+//   dir: './', // Path to your Next.js app directory
+// });
 
-const customJestConfig = {
-  testEnvironment: 'jest-environment-jsdom',
-  // Remove or comment out the setupFilesAfterEnv if not needed
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-};
+// const customJestConfig = {
+//   testEnvironment: 'jest-environment-jsdom',
+//   // Remove or comment out the setupFilesAfterEnv if not needed
+//   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+// };
 
-module.exports = createJestConfig(customJestConfig);
+// module.exports = createJestConfig(customJestConfig);
